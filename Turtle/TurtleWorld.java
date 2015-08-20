40be330f08fcf60a5f07088b51c6742f5d398b3c
jelemanski <jelemanski@NN121-HA-13.HS.sd203.org>
2015-08-20 10:30:20 -0500

    jimmy's turtle

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -6,8 +6,18 @@ public class TurtleWorld
     {
         World turtleWorld = new World();
         Turtle turtle = new Turtle(turtleWorld);
-        turtle.setPenColor(Color.RED);
+        turtle.setPenColor(Color.GREEN);
         turtle.penDown();
         turtle.forward(50);
+        turtle.penUp();
+        turtle.forward(25);
+        turtle.penDown();
+        turtle.forward();
+        turtle.turnRight();
+        turtle.forward(25);
+        turtle.turnRight();
+        turtle.forward(112);
+        turtle.turnRight();
+        turtle.forward();
     }
 }
